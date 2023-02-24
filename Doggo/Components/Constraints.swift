153f99d020bed645caf9b805603b0c244da0a05a
Luiz Henrique <luiz.sousa>
2023-02-24 16:43:24 +0000

    Added Network layer with basic DogApi

---
+++ Doggo/Components/Constraints.swift
@@ -0,0 +1,12 @@
+//
+//  Constraints.swift
+//  Doggo
+//
+//  Created by Luiz Henrique on 24/02/2023.
+//
+
+import Foundation
+// API Constraints
+
+let baseURL = "https://api.thedogapi.com/v1/"
+let API_KEY = "MHsZ52zZffWPFlIs8xkKGOiYQpw66O86"
---
+++ Doggo/Components/Network.swift
@@ -0,0 +1,93 @@
+//
+//  Network.swift
+//  Doggo
+//
+//  Created by Luiz Henrique on 24/02/2023.
+//
+
+import Foundation
+import Alamofire
+
+class Network {
+    
+    enum Result<Value, Error: Swift.Error> {
+        case success(Data)
+        case failure(Error)
+    }
+    
+    typealias Handler = (Result<Data, Error>) -> Void
+    
+    
+    static let shared: Network = Network()
+    
+    private init() {
+        manager = Alamofire.Session()
+    }
+    
+   
+    private let manager: Alamofire.Session!
+    
+    enum Endpoint {
+        case list
+        case search
+        
+        internal var method: HTTPMethod {
+            switch self {
+            case .list      : return .get
+            case .search  : return .get
+            }
+        }
+        
+        fileprivate var URL: String{
+            switch self {
+                
+            case .list:
+                return "images/search?size=small&mime_types=jpg&order=ASC&limit=10&page=%@"
+            case .search:
+                return "images/search?breed_ids=%@size=small&mime_types=jpg&order=ASC&limit=10&page=%@"
+            }
+            
+        }
+        
+        fileprivate var headers: HTTPHeaders {
+            switch self {
+            default:
+                return [
+                    "Accept": "application/json",
+                    "x-api-key": API_KEY
+                ]
+                
+            }
+        }
+        
+    }
+    
+    func request(_ endpoint        : Endpoint,
+                 _ pathParamenters : [CVarArg]?,
+                 _ parameters      : [String:Any]?,
+                 handler: @escaping Handler) {
+        
+        var requestURL = baseURL + endpoint.URL
+        
+        if pathParamenters != nil && (pathParamenters?.count)! > 0 {
+            requestURL = String.init(format: requestURL, arguments: pathParamenters!)
+        }
+        
+        let encoding: ParameterEncoding = JSONEncoding.default
+        manager.request(requestURL, method: endpoint.method, parameters: parameters, encoding: encoding, headers: endpoint.headers)
+            .responseJSON(completionHandler: { response in
+                switch response.result {
+                case .success(_):
+                    if let data = response.data {
+                        handler(.success(data))
+                    }
+                    
+                case .failure(let error):
+                    handler(.failure(error))
+                    
+                }
+            })
+        
+    }
+    
+}
